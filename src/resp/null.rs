f551b175bde3864f7ba1086d6b066dad762bdc07
uynhg <tim.engbrocks@student.kit.edu>
2024-03-17 20:00:28 +0100

    7th stage; codecrafters uses RESP2 not RESP3

--- src/resp/null.rs
+++ src/resp/null.rs
@@ -7,7 +7,12 @@ pub struct RespNull {}
 
 impl RespObject for RespNull {
 	fn serialize(&self) -> String {
-		format!("_{}", RESP_TERMINATOR)
+		// NOTE: CodeCrafters tests with RESP2 which uses null bulk string replys for gets that do not return a value.
+		// The null bulk string reply has already been extracted into its own value,
+		// because representing a null bulk string reply as its own value is easier to implement and is the case
+		// for future versions of RESP.
+		// TODO: Maybe switch to RESP3 in the future which uses the following special null value: _\r\n
+		format!("$-1{}", RESP_TERMINATOR)
 	}
 
 	fn deserialize(data: &str) -> (usize, RespValues) {
